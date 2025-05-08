6593d406c79c8c04dd1fb0d4c04092f19277e912
MaxSouza88 <souza1988@hotmail.com>
2025-05-08 10:27:36 -0300

    alterando front-end

--- src/main/java/com/CadastroSimples/Cadastro/PessoaController/PessoaController.java
+++ src/main/java/com/CadastroSimples/Cadastro/PessoaController/PessoaController.java
@@ -3,14 +3,13 @@ package com.CadastroSimples.Cadastro.PessoaController;
 import com.CadastroSimples.Cadastro.Dto.ResponsePessoaDto;
 import com.CadastroSimples.Cadastro.Entities.Pessoa;
 import com.CadastroSimples.Cadastro.Service.PessoaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/pessoas")
 public class PessoaController {
 
